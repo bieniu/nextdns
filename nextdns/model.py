039a916f65fb938e224f354aa2e7bd1e7175c9ae
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-06-11 12:01:45 +0200

    Some changes in model

--- nextdns/model.py
+++ nextdns/model.py
@@ -10,14 +10,15 @@ class AnalyticsStatus:
     blocked_queries: int = 0
     allowed_queries: int = 0
     all_queries: int = 0
-    blocked_percentage: float = 0
+    blocked_queries_ratio: float = 0
 
     def __post_init__(self) -> None:
         """Call after initialization."""
         self.all_queries = sum(
             [self.default_queries, self.blocked_queries, self.allowed_queries]
         )
-        self.blocked_percentage = (
+
+        self.blocked_queries_ratio = (
             0
             if not self.all_queries
             else round(self.blocked_queries / self.all_queries * 100, 1)
@@ -30,12 +31,13 @@ class AnalyticsDnssec:
 
     not_validated_queries: int = 0
     validated_queries: int = 0
-    validated_percentage: float = 0
+    validated_queries_ratio: float = 0
 
     def __post_init__(self) -> None:
         """Call after initialization."""
         all_queries = sum([self.validated_queries, self.not_validated_queries])
-        self.validated_percentage = (
+
+        self.validated_queries_ratio = (
             0
             if not all_queries
             else round(self.validated_queries / all_queries * 100, 1)
@@ -48,12 +50,13 @@ class AnalyticsEncrypted:
 
     encrypted_queries: int = 0
     unencrypted_queries: int = 0
-    encrypted_percentage: float = 0
+    encrypted_queries_ratio: float = 0
 
     def __post_init__(self) -> None:
         """Call after initialization."""
         all_queries = sum([self.encrypted_queries, self.unencrypted_queries])
-        self.encrypted_percentage = (
+
+        self.encrypted_queries_ratio = (
             0
             if not all_queries
             else round(self.encrypted_queries / all_queries * 100, 1)
@@ -66,12 +69,13 @@ class AnalyticsIpVersions:
 
     ipv6_queries: int = 0
     ipv4_queries: int = 0
-    ipv6_percentage: float = 0
+    ipv6_queries_ratio: float = 0
 
     def __post_init__(self) -> None:
         """Call after initialization."""
         all_queries = sum([self.ipv6_queries, self.ipv4_queries])
-        self.ipv6_percentage = (
+
+        self.ipv6_queries_ratio = (
             0 if not all_queries else round(self.ipv6_queries / all_queries * 100, 1)
         )
 
@@ -82,7 +86,24 @@ class AnalyticsProtocols:
 
     doh_queries: int = 0
     dot_queries: int = 0
-    udp_queries: float = 0
+    udp_queries: int = 0
+    doh_queries_ratio: float = 0
+    dot_queries_ratio: float = 0
+    udp_queries_ratio: float = 0
+
+    def __post_init__(self) -> None:
+        """Call after initialization."""
+        all_queries = sum([self.doh_queries, self.dot_queries, self.udp_queries])
+
+        self.doh_queries_ratio = (
+            0 if not all_queries else round(self.doh_queries / all_queries * 100, 1)
+        )
+        self.dot_queries_ratio = (
+            0 if not all_queries else round(self.dot_queries / all_queries * 100, 1)
+        )
+        self.udp_queries_ratio = (
+            0 if not all_queries else round(self.udp_queries / all_queries * 100, 1)
+        )
 
 
 @dataclass
