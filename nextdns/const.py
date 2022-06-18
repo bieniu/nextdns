f44d154f0785b917def00394b38f624207ac1ff0
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-06-18 11:20:26 +0200

    Cleaning

--- nextdns/const.py
+++ nextdns/const.py
@@ -21,7 +21,7 @@ MAP_IP_VERSIONS = {4: "ipv4_queries", 6: "ipv6_queries"}
 MAP_PROFILE = {"parentalControl": "parental_control"}
 MAP_PROTOCOLS = {
     "DNS-over-HTTPS": "doh_queries",
-    "DNS-over-QUIC": "dquic_queries",
+    "DNS-over-QUIC": "doq_queries",
     "DNS-over-TLS": "dot_queries",
     "UDP": "udp_queries",
 }
