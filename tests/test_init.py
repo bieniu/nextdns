bc8607ac50ca1101a691736a7f2b848f58cd1e32
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-07-03 15:41:38 +0200

    Improve test coverage

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,5 +1,6 @@
 """Tests for nextdns package."""
 import json
+from http import HTTPStatus
 
 import aiohttp
 import pytest
@@ -7,14 +8,21 @@ from aioresponses import aioresponses
 
 from nextdns import (
     ATTR_ANALYTICS,
+    ATTR_CLEAR_LOGS,
     ATTR_PROFILE,
     ATTR_PROFILES,
     ATTR_TEST,
     ENDPOINTS,
+    ApiError,
+    InvalidApiKeyError,
     NextDns,
     ProfileIdNotFoundError,
     ProfileNameNotFoundError,
+    SettingNotSupportedError,
 )
+from nextdns.const import ATTR_BLOCK_PAGE
+
+PROFILE_ID = "fakepr"
 
 
 @pytest.mark.asyncio
@@ -37,54 +45,52 @@ async def test_valid_data():  # pylint: disable=too-many-locals,too-many-stateme
     with open("tests/fixtures/profile.json", encoding="utf-8") as file:
         profile_data = json.load(file)
 
-    profile_id = "fakepr"
-
     session = aiohttp.ClientSession()
 
     with aioresponses() as session_mock:
         session_mock.get(ENDPOINTS[ATTR_PROFILES], payload=profiles_data)
         session_mock.get(
-            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=profile_id, type="dnssec"),
+            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=PROFILE_ID, type="dnssec"),
             payload=dnssec_data,
         )
         session_mock.get(
-            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=profile_id, type="encryption"),
+            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=PROFILE_ID, type="encryption"),
             payload=encryption_data,
         )
         session_mock.get(
-            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=profile_id, type="ipVersions"),
+            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=PROFILE_ID, type="ipVersions"),
             payload=ip_versions_data,
         )
         session_mock.get(
-            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=profile_id, type="protocols"),
+            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=PROFILE_ID, type="protocols"),
             payload=protocols_data,
         )
         session_mock.get(
-            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=profile_id, type="status"),
+            ENDPOINTS[ATTR_ANALYTICS].format(profile_id=PROFILE_ID, type="status"),
             payload=status_data,
         )
         session_mock.get(
-            ENDPOINTS[ATTR_TEST].format(profile_id=profile_id), payload=test_data
+            ENDPOINTS[ATTR_TEST].format(profile_id=PROFILE_ID), payload=test_data
         )
         session_mock.get(
-            ENDPOINTS[ATTR_PROFILE].format(profile_id=profile_id), payload=profile_data
+            ENDPOINTS[ATTR_PROFILE].format(profile_id=PROFILE_ID), payload=profile_data
         )
 
         nextdns = await NextDns.create(session, "fakeapikey")
 
-        analitycs = await nextdns.get_all_analytics(profile_id)
+        analitycs = await nextdns.get_all_analytics(PROFILE_ID)
         dnssec = analitycs.dnssec
         encryption = analitycs.encryption
         ip_versions = analitycs.ip_versions
         protocols = analitycs.protocols
         status = analitycs.status
-        connection_status = await nextdns.connection_status(profile_id)
-        settings = await nextdns.get_settings(profile_id)
+        connection_status = await nextdns.connection_status(PROFILE_ID)
+        settings = await nextdns.get_settings(PROFILE_ID)
 
     await session.close()
 
     assert len(nextdns.profiles) == 1
-    assert nextdns.profiles[0].id == "fakepr"
+    assert nextdns.profiles[0].id == PROFILE_ID
     assert nextdns.profiles[0].fingerprint == "fakeprofile12"
     assert nextdns.profiles[0].name == "Fake Profile"
 
@@ -117,7 +123,7 @@ async def test_valid_data():  # pylint: disable=too-many-locals,too-many-stateme
     assert status.blocked_queries_ratio == 38.5
 
     assert connection_status.connected is True
-    assert connection_status.profile_id == profile_id
+    assert connection_status.profile_id == PROFILE_ID
 
     assert settings.block_page is False
     assert settings.cache_boost is True
@@ -143,8 +149,8 @@ async def test_valid_data():  # pylint: disable=too-many-locals,too-many-stateme
     assert settings.safesearch is False
     assert settings.youtube_restricted_mode is False
 
-    assert nextdns.get_profile_name(profile_id) == "Fake Profile"
-    assert nextdns.get_profile_id("Fake Profile") == profile_id
+    assert nextdns.get_profile_name(PROFILE_ID) == "Fake Profile"
+    assert nextdns.get_profile_id("Fake Profile") == PROFILE_ID
 
 
 @pytest.mark.asyncio
@@ -187,3 +193,108 @@ async def test_profile_name_not_found():
         nextdns.get_profile_id("Profile Name")
     except Exception as exc:  # pylint: disable=broad-except
         assert isinstance(exc, ProfileNameNotFoundError) is True
+
+
+@pytest.mark.asyncio
+async def test_clear_logs():
+    """Test clear_logs() method."""
+    with open("tests/fixtures/profiles.json", encoding="utf-8") as file:
+        profiles_data = json.load(file)
+
+    session = aiohttp.ClientSession()
+
+    with aioresponses() as session_mock:
+        session_mock.get(ENDPOINTS[ATTR_PROFILES], payload=profiles_data)
+        session_mock.delete(
+            ENDPOINTS[ATTR_CLEAR_LOGS].format(profile_id=PROFILE_ID),
+            status=HTTPStatus.NO_CONTENT.value,
+        )
+
+        nextdns = await NextDns.create(session, "fakeapikey")
+
+        result = await nextdns.clear_logs(PROFILE_ID)
+
+    await session.close()
+
+    assert result is True
+
+
+@pytest.mark.asyncio
+async def test_set_setting():
+    """Test set_setting() method."""
+    with open("tests/fixtures/profiles.json", encoding="utf-8") as file:
+        profiles_data = json.load(file)
+
+    session = aiohttp.ClientSession()
+
+    with aioresponses() as session_mock:
+        session_mock.get(ENDPOINTS[ATTR_PROFILES], payload=profiles_data)
+        session_mock.patch(
+            ENDPOINTS[ATTR_BLOCK_PAGE].format(profile_id=PROFILE_ID),
+            status=HTTPStatus.NO_CONTENT.value,
+        )
+
+        nextdns = await NextDns.create(session, "fakeapikey")
+
+        result = await nextdns.set_setting(PROFILE_ID, "block_page", True)
+
+    await session.close()
+
+    assert result is True
+
+
+@pytest.mark.asyncio
+async def test_set_not_supported_setting():
+    """Test set_setting() method with not supported setting."""
+    with open("tests/fixtures/profiles.json", encoding="utf-8") as file:
+        profiles_data = json.load(file)
+
+    session = aiohttp.ClientSession()
+
+    with aioresponses() as session_mock:
+        session_mock.get(ENDPOINTS[ATTR_PROFILES], payload=profiles_data)
+
+        nextdns = await NextDns.create(session, "fakeapikey")
+
+        try:
+            await nextdns.set_setting(PROFILE_ID, "unsupported_setting", True)
+        except Exception as exc:  # pylint: disable=broad-except
+            assert isinstance(exc, SettingNotSupportedError) is True
+
+    await session.close()
+
+
+@pytest.mark.asyncio
+async def test_invalid_api_key():
+    """Test error when provided API key is invalid."""
+    session = aiohttp.ClientSession()
+
+    with aioresponses() as session_mock:
+        session_mock.get(ENDPOINTS[ATTR_PROFILES], status=HTTPStatus.FORBIDDEN.value)
+
+        try:
+            await NextDns.create(session, "fakeapikey")
+        except Exception as exc:  # pylint: disable=broad-except
+            assert isinstance(exc, InvalidApiKeyError) is True
+
+    await session.close()
+
+
+@pytest.mark.asyncio
+async def test_api_error():
+    """Test API error."""
+    session = aiohttp.ClientSession()
+
+    with aioresponses() as session_mock:
+        session_mock.get(
+            ENDPOINTS[ATTR_PROFILES],
+            status=HTTPStatus.BAD_REQUEST.value,
+            payload={"errors": [{"code": "badRequest"}]},
+        )
+
+        try:
+            await NextDns.create(session, "fakeapikey")
+        except ApiError as exc:
+            assert str(exc.status) == "400, badRequest"
+
+    await session.close()
