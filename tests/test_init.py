585136ae5e57591a272ed4b33bc83417c7a236ea
Maciej Bieniek <bieniu@users.noreply.github.com>
2024-04-16 16:38:20 +0200

    Use snapshots in tests (#337)
    Co-authored-by: Maciej Bieniek <478555+bieniu@users.noreply.github.com>

--- tests/test_init.py
+++ tests/test_init.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import aiohttp
 import pytest
 from aioresponses import aioresponses
+from syrupy import SnapshotAssertion
 
 from nextdns import (
     ATTR_ANALYTICS,
@@ -34,7 +35,7 @@ PROFILE_ID = "fakepr"
 
 
 @pytest.mark.asyncio()
-async def test_valid_data() -> None:
+async def test_valid_data(snapshot: SnapshotAssertion) -> None:
     """Test with valid data."""
     with Path.open("tests/fixtures/profiles.json", encoding="utf-8") as file:
         profiles_data = json.load(file)
@@ -87,136 +88,18 @@ async def test_valid_data() -> None:
         nextdns = await NextDns.create(session, "fakeapikey")
 
         analitycs = await nextdns.get_all_analytics(PROFILE_ID)
-        dnssec = analitycs.dnssec
-        encryption = analitycs.encryption
-        ip_versions = analitycs.ip_versions
-        protocols = analitycs.protocols
-        status = analitycs.status
         connection_status = await nextdns.connection_status(PROFILE_ID)
         settings = await nextdns.get_settings(PROFILE_ID)
 
     await session.close()
 
-    assert len(nextdns.profiles) == 1
-    assert nextdns.profiles[0].id == PROFILE_ID
-    assert nextdns.profiles[0].fingerprint == "fakeprofile12"
-    assert nextdns.profiles[0].name == "Fake Profile"
+    assert nextdns == snapshot
+    assert analitycs == snapshot
+    assert connection_status == snapshot
+    assert settings == snapshot
 
-    assert dnssec.not_validated_queries == 793765
-    assert dnssec.validated_queries == 49451
-    assert dnssec.validated_queries_ratio == 5.9
-
-    assert encryption.encrypted_queries == 1380260
-    assert encryption.unencrypted_queries == 40
-    assert encryption.encrypted_queries_ratio == 100.0
-
-    assert ip_versions.ipv6_queries == 42117
-    assert ip_versions.ipv4_queries == 1338183
-    assert ip_versions.ipv6_queries_ratio == 3.1
-
-    assert protocols.doh_queries == 99999
-    assert protocols.doh3_queries == 88888
-    assert protocols.doq_queries == 55555
-    assert protocols.dot_queries == 101010
-    assert protocols.tcp_queries == 33333
-    assert protocols.udp_queries == 44444
-    assert protocols.doh_queries_ratio == 23.6
-    assert protocols.doh3_queries_ratio == 21.0
-    assert protocols.doq_queries_ratio == 13.1
-    assert protocols.dot_queries_ratio == 23.9
-    assert protocols.tcp_queries_ratio == 7.9
-    assert protocols.udp_queries_ratio == 10.5
-
-    assert status.all_queries == 1380300
-    assert status.allowed_queries == 5452
-    assert status.blocked_queries == 530805
-    assert status.default_queries == 837764
-    assert status.relayed_queries == 6279
-    assert status.blocked_queries_ratio == 38.5
-
-    assert connection_status.connected is True
-    assert connection_status.profile_id == PROFILE_ID
-
-    assert settings.block_page is False
-    assert settings.cache_boost is True
-    assert settings.cname_flattening is True
-    assert settings.anonymized_ecs is True
-    assert settings.logs is True
-    assert settings.logs_location == "ch"
-    assert settings.logs_retention == 720
-    assert settings.web3 is True
-    assert settings.allow_affiliate is True
-    assert settings.block_disguised_trackers is True
-    assert settings.ai_threat_detection is True
-    assert settings.block_csam is True
-    assert settings.block_ddns is True
-    assert settings.block_nrd is True
-    assert settings.block_parked_domains is True
-    assert settings.cryptojacking_protection is True
-    assert settings.dga_protection is True
-    assert settings.dns_rebinding_protection is True
-    assert settings.google_safe_browsing is True
-    assert settings.idn_homograph_attacks_protection is True
-    assert settings.threat_intelligence_feeds is True
-    assert settings.typosquatting_protection is True
-    assert settings.block_bypass_methods is True
-    assert settings.safesearch is False
-    assert settings.youtube_restricted_mode is False
-
-    assert settings.block_9gag is False
-    assert settings.block_amazon is False
-    assert settings.block_bereal is False
-    assert settings.block_blizzard is False
-    assert settings.block_chatgpt is False
-    assert settings.block_dailymotion is False
-    assert settings.block_discord is False
-    assert settings.block_disneyplus is False
-    assert settings.block_ebay is False
-    assert settings.block_facebook is False
-    assert settings.block_fortnite is False
-    assert settings.block_google_chat is False
-    assert settings.block_hbomax is False
-    assert settings.block_hulu is False
-    assert settings.block_imgur is False
-    assert settings.block_instagram is False
-    assert settings.block_leagueoflegends is False
-    assert settings.block_mastodon is False
-    assert settings.block_messenger is False
-    assert settings.block_minecraft is False
-    assert settings.block_netflix is False
-    assert settings.block_pinterest is False
-    assert settings.block_playstation_network is False
-    assert settings.block_primevideo is False
-    assert settings.block_reddit is False
-    assert settings.block_roblox is False
-    assert settings.block_signal is False
-    assert settings.block_skype is False
-    assert settings.block_snapchat is False
-    assert settings.block_spotify is False
-    assert settings.block_steam is False
-    assert settings.block_telegram is False
-    assert settings.block_tiktok is False
-    assert settings.block_tinder is False
-    assert settings.block_tumblr is False
-    assert settings.block_twitch is False
-    assert settings.block_twitter is False
-    assert settings.block_vimeo is False
-    assert settings.block_vk is False
-    assert settings.block_whatsapp is False
-    assert settings.block_xboxlive is False
-    assert settings.block_youtube is False
-    assert settings.block_zoom is False
-
-    assert settings.block_dating is True
-    assert settings.block_gambling is True
-    assert settings.block_piracy is True
-    assert settings.block_porn is True
-    assert settings.block_social_networks is False
-    assert settings.block_online_gaming is False
-    assert settings.block_video_streaming is False
-
-    assert nextdns.get_profile_name(PROFILE_ID) == "Fake Profile"
-    assert nextdns.get_profile_id("Fake Profile") == PROFILE_ID
+    assert nextdns.get_profile_name(PROFILE_ID) == snapshot
+    assert nextdns.get_profile_id("Fake Profile") == snapshot
 
 
 @pytest.mark.asyncio()
