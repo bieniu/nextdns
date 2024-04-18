d7e8439221fc9b9a27efa24be13e824e09e802a2
Maciej Bieniek <bieniu@users.noreply.github.com>
2024-04-18 21:39:35 +0200

    Use test fixtures (#339)
    Co-authored-by: Maciej Bieniek <478555+bieniu@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,20 @@
 """Set up some common test helper things."""
 
+import json
 from pathlib import Path
+from typing import Any
 
 import pytest
 from syrupy.assertion import SnapshotAssertion
 from syrupy.extensions.amber import AmberSnapshotExtension
 from syrupy.location import PyTestLocation
+
+
+@pytest.fixture()
+def profiles_data() -> dict[str, Any]:
+    """Return the profiles data from the fixture file."""
+    with Path.open("tests/fixtures/profiles.json", encoding="utf-8") as file:
+        return json.load(file)
 
 
 @pytest.fixture()
