78b505bc51922906b3a530234339f3fe344dc70a
Maciej Bieniek <478555+bieniu@users.noreply.github.com>
2024-06-27 00:10:01 +0200

    Tweak retry

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "3.1.2"
+VERSION = "3.1.3"
 
 setup(
     name="nextdns",
