500f055c8b624da0c2920ed98023c8fe3d98e409
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-01-18 17:04:22 +0100

    Update devcontainer config

--- scripts/setup-local-env.sh
+++ scripts/setup-local-env.sh
@@ -2,9 +2,9 @@
 
 PYTHON_VERSION=3.14
 
-python$PYTHON_VERSION -m pip install uv
-python$PYTHON_VERSION -m uv venv venv --seed --python=$PYTHON_VERSION
+python$PYTHON_VERSION -m pip install uv --upgrade
+python$PYTHON_VERSION -m uv venv venv --clear --seed --python=$PYTHON_VERSION
 source venv/bin/activate
 pip install uv
-uv pip install . .[dev]
+uv sync --all-groups --active
 prek install
